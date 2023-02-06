ad382e43f966ca757b0c8a0ddb371f90648a0445
Natalya <pauls88@yandex.ru>
2023-02-06 22:12:04 +0500

    Addnewfile

---
+++ 31012023-2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Напишите программу, которая найдёт точку пересечения двух прямых,");
+Console.WriteLine("заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.");
+Console.WriteLine();
+
+Console.WriteLine("Для решения задачи введите исходные данные: ");
+Console.Write("k1 =");
+double k1 =(float) Convert.ToDouble(Console.ReadLine());
+Console.Write("b1 =");
+double b1= (float) Convert.ToDouble(Console.ReadLine());
+Console.Write("k2 =");
+double k2= (float) Convert.ToDouble(Console.ReadLine());
+Console.Write("b2 =");
+double b2= (float) Convert.ToDouble(Console.ReadLine());
+if ((k1 == k2) && (b1 == b2))
+{
+  Console.Write("Заданные прямые совпадают.Они имеют бесконечное множество общих точек.");
+}
+else if (k1==k2)
+{
+  Console.Write("Заданные прямые параллельные. Они не имеют точек пересечения");
+}
+else
+{   double x , y;
+    x=(b2-b1)/(k1-k2);
+    y=(k1*(b2-b1))/(k1-k2)+b1;
+
+    Console.Write($"Точка пересечения прямых имеет координаты X={x.ToString("##.000")}, Y={y.ToString("##.000")}.");
+}
---
+++ 3102023-1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+Console.WriteLine();
+
+Console.Write("Введите числовой ряд (через пробел): ");
+int[] positivAttay = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse); // строка с консоли Console.ReadLine(), метод Split() получаем из этой строки массив, в качестве разделителей-пробельные символы.
+//метод Array.ConvertAll применяет ко всем элементам входного массива функцию, переданную вторым параметром (int.Parse),получается массив int
+int count = 0; 
+
+for (int i = 0; i < positivAttay.Length; i++)
+{
+    if (positivAttay[i] > 0)
+    { 
+        count++;
+    } 
+}
+Console.WriteLine($"Из {positivAttay.Length} введенных пользователем чисел {count} положительные.");
